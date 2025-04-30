b36f2f81e4fa64728da8ab5eb684086359ca7c89
Dino234118 <jkml9468@gmail.com>
2025-04-30 13:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 import pickle
 import pandas as pd
 import numpy as np
